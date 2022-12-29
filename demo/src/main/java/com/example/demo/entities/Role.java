3358490ee1720489fb458f0cd777c9dce4a58ee7
palash <p30alakgupta@gmail.com>
2022-12-29 11:07:35 +0530

    first commit

---
+++ demo/src/main/java/com/example/demo/entities/Role.java
@@ -0,0 +1,24 @@
+package com.example.demo.entities;
+
+import java.util.*;
+
+import com.example.demo.entities.Users.User;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "roles")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false, unique = true)
+    private String name;
+    @ManyToMany(mappedBy = "roles")
+    private List<User> users = new ArrayList<>();
+}
